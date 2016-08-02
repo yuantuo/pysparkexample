6a63b80f0574aee42259f8746a865ec833ce11b4
tok <tok_apple2009@hotmail.co.uk>
2016-08-02 09:41:47 +0100

    star unpacking

--- sparky/test.py
+++ sparky/test.py
@@ -1,6 +1,7 @@
 class Unpacking:
 
     def unpack(self):
+
         p = (4,5)
         x,y = p
         print("%s, %s" % (x, y))
@@ -10,15 +11,61 @@ class Unpacking:
         #if number of unpack item is not correct is will give ValueError
         print("%s,%s,%d,%d,%d,%d" % (a,b,c,d[0],d[1],d[2]))
 
-        #unpack a string
+        """
+            unpack a string
+        """
         p = 'Hello'
         a,b,c,d,e = p
         print(a)
 
+
+        """
+            disgard vaiable with _
+        """
         p = ['foo', 'bar', 123, (2016,8,2)]
-        #disgard vaiable with _
         _,b,c,_ = p
         print(b) #print _ return nothing
+
+        """
+            Using *
+        """
+
+        p = ['foo', 'bar', '234234213434', '3421342341324']
+        a,c,*num=p
+        print(a)
+        print(*num)
+
+
+        """
+         unpacking dictionary
+        """
+
+        records = [
+            ('foo', 1, 2),
+            ('bar', 'hello'),
+            ('foo', 3, 4),
+        ]
+
+        def do_foo(x,y):
+            print('foo', x, y)
+
+        def do_bar(s):
+            print('bar', s)
+
+        for tag, *args in records:
+            if tag == 'foo':
+                do_foo(*args)
+            else:
+                do_bar(*args)
+
+
+        line = 'nobody:*:-2:-2:Unprivileged User:/var/empty:/usr/bin/false'
+
+        name,*_,home,bin = line.split(':')
+        print(name)
+        print(home)
+        print(bin)
+
 
 
 
